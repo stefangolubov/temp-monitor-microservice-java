37a4b9a27852a2e5beadd2912220c802155807a7
Stefan.Golubov <stefan.golubov@qinshift.com>
2025-07-17 16:45:07 +0200

    Adding pom and banner. Renaming main application class name

--- temp-monitor-microservice-java/src/main/java/com/tempmonitor/TempMonitorApplication.java
+++ temp-monitor-microservice-java/src/main/java/com/tempmonitor/TempMonitorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TempMonitorMicroserviceJavaApplication {
+public class TempMonitorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TempMonitorMicroserviceJavaApplication.class, args);
+		SpringApplication.run(TempMonitorApplication.class, args);
 	}
 
 }
